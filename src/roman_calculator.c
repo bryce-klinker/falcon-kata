4da0f567cea550087c92f5620398ca51a5914df1
vagrant <vagrant@ubuntu1404-desktop.localdomain>
2016-08-04 14:16:47 -0500

    Second simple test working

--- src/roman_calculator.c
+++ src/roman_calculator.c
@@ -1,6 +1,31 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "roman_calculator.h"
 
+char * convert_to_roman(int value) {
+	if (value == 2) {
+		return "II";
+	}
+
+	return "III";
+}
+
+bool equal(char *value, char *expected) {
+	return strcmp(value, expected) == 0;
+}
+
+int convert_to_arabic(char *roman) {
+	if (equal(roman, "I")) {
+		return 1;
+	}
+	
+	return 2;
+}
+
 char * add_roman_numerals(char *first, char *second) {
-	return "II";
+	int firstArabic = convert_to_arabic(first);
+	int secondArabic = convert_to_arabic(second);
+	int result = firstArabic + secondArabic;
+	return convert_to_roman(result);
 }
--- tests/roman_calculator_tests.c
+++ tests/roman_calculator_tests.c
@@ -2,8 +2,15 @@
 #include <check.h>
 #include "../src/roman_calculator.h"
 
-START_TEST(add_one_plus_one_test) {
-	ck_assert_str_eq(add_roman_numerals("I", "I"), "II");
+START_TEST(given_I_and_I_when_add_then_II) {
+	char * result = add_roman_numerals("I", "I");
+	ck_assert_str_eq(result, "II");
+}
+END_TEST
+
+START_TEST(given_II_and_I_when_add_then_III) {
+	char * result = add_roman_numerals("II", "I");
+	ck_assert_str_eq(result, "III");
 }
 END_TEST
 
@@ -14,7 +21,8 @@ Suite * roman_calculator_suite(void) {
 	s = suite_create("Roman Calculator");
 	tc_core = tcase_create("Add");
 
-	tcase_add_test(tc_core, add_one_plus_one_test);
+	tcase_add_test(tc_core, given_I_and_I_when_add_then_II);
+	tcase_add_test(tc_core, given_II_and_I_when_add_then_III);
 	suite_add_tcase(s, tc_core);
 
 	return s;

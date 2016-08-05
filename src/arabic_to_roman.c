0beac24bbaf7fb7f500e77ef84f99a216607fea3
vagrant <vagrant@ubuntu1404-desktop.localdomain>
2016-08-05 13:11:29 -0500

    Adds working for simple numbers

--- src/arabic_to_roman.c
+++ src/arabic_to_roman.c
@@ -2,29 +2,23 @@
 #include <string.h>
 #include "roman_arabic_map.h"
 
-char *get_roman_string(RomanArabicMap map) {
-	char *string = (char *)malloc(3);
-	string[0] = map.roman[0];
-	if (map.roman[1] != NA) {
-		string[1] = map.roman[1];
-	}
-	return string;
-}
-
 char *to_roman(int arabic) {
 	char *roman;
 	roman = (char *) malloc(1000);
-	for(int i = 0; i < get_maps_size(); ++i) {
-		struct RomanArabicMap map = roman_arabic_maps[i]; 
-		int remainder = arabic - map.arabic; 
-		if(remainder >= 0) {
-			arabic = remainder;
-			strcpy(roman, get_roman_string(map));
-		}		
-
-		if (arabic == 0) {
-			break;
+	int currentIndex = 0;
+	while(arabic != 0) {
+		for(int i = 0; i < get_maps_size(); ++i) {
+			struct RomanArabicMap map = roman_arabic_maps[i]; 
+			int remainder = arabic - map.arabic; 
+			if(remainder >= 0) {
+				arabic = remainder;
+				roman[currentIndex++] = map.roman[0];
+				if (map.roman[1] != NA) {
+					roman[currentIndex++] = map.roman[1];
+				}
+			}
 		}
 	}
+	roman[currentIndex] = '\0';
 	return roman;
 }

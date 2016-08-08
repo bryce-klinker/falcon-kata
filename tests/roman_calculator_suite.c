88768e465ddfedd88ba94f0e6c470c33664ee421
vagrant <vagrant@ubuntu1404-desktop.localdomain>
2016-08-08 10:57:37 -0500

    Added tests for add second parameter with too many X's

--- tests/roman_calculator_suite.c
+++ tests/roman_calculator_suite.c
@@ -42,21 +42,27 @@ START_TEST(given_IIII_and_I_when_add_then_IIII_is_not_valid) {
 }
 END_TEST
 
+START_TEST(given_I_and_IIII_when_add_then_IIII_is_not_valid) {
+	char * result = add_roman_numerals("I", "IIII");
+	ck_assert_str_eq(result, "Second roman numeral is not valid");
+}
+END_TEST
+
 START_TEST(given_XXXX_and_I_when_add_then_XXXX_is_not_valid) {
 	char * result = add_roman_numerals("XXXX", "I");
 	ck_assert_str_eq(result, "First roman numeral is not valid");
 }
 END_TEST
 
-START_TEST(given_MMMM_and_I_when_add_then_MMMM_is_not_valid) {
-	char * result = add_roman_numerals("MMMM", "I");
-	ck_assert_str_eq(result, "First roman numeral is not valid");
+START_TEST(given_I_and_XXXX_when_add_then_XXXX_is_not_valid) {
+	char * result = add_roman_numerals("I", "XXXX");
+	ck_assert_str_eq(result, "Second roman numeral is not valid");
 }
 END_TEST
 
-START_TEST(given_I_and_IIII_when_add_then_IIII_is_not_valid) {
-	char * result = add_roman_numerals("I", "IIII");
-	ck_assert_str_eq(result, "Second roman numeral is not valid");
+START_TEST(given_MMMM_and_I_when_add_then_MMMM_is_not_valid) {
+	char * result = add_roman_numerals("MMMM", "I");
+	ck_assert_str_eq(result, "First roman numeral is not valid");
 }
 END_TEST
 
@@ -137,6 +143,7 @@ TCase *create_add_test_case() {
 	tcase_add_test(add_case, given_IIII_and_I_when_add_then_IIII_is_not_valid);
 	tcase_add_test(add_case, given_I_and_IIII_when_add_then_IIII_is_not_valid);
 	tcase_add_test(add_case, given_XXXX_and_I_when_add_then_XXXX_is_not_valid);	
+	tcase_add_test(add_case, given_I_and_XXXX_when_add_then_XXXX_is_not_valid);	
 	tcase_add_test(add_case, given_MMMM_and_I_when_add_then_MMMM_is_not_valid);
 	tcase_add_test(add_case, given_I_and_MMMM_when_add_then_MMMM_is_not_valid);
 	return add_case;

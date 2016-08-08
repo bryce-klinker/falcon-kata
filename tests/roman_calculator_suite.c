1a21d0750192d3c0db6adeb0b3e5c9abd4de040d
vagrant <vagrant@ubuntu1404-desktop.localdomain>
2016-08-08 09:59:23 -0500

    Added test to verify subtract ensures there aren't too many M's

--- tests/roman_calculator_suite.c
+++ tests/roman_calculator_suite.c
@@ -107,6 +107,12 @@ START_TEST(given_I_and_IIII_when_subtract_then_IIII_is_not_valid) {
 }
 END_TEST
 
+START_TEST(given_MMMM_and_I_when_subtract_then_MMMM_is_not_valid) {
+	char * result = subtract_roman_numerals("MMMM", "I");
+	ck_assert_str_eq(result, "First roman numeral is not valid");
+}
+END_TEST
+
 TCase *create_add_test_case() {
 	TCase *add_case;
 	add_case = tcase_create("Add");
@@ -134,6 +140,7 @@ TCase *create_subtract_test_case() {
 	tcase_add_test(subtract_case, given_I_and_II_when_subtract_then_invalid_subtraction_second_number_is_greater_than_first_number);
 	tcase_add_test(subtract_case, given_IIII_and_I_when_subtract_then_IIII_is_not_valid);
 	tcase_add_test(subtract_case, given_I_and_IIII_when_subtract_then_IIII_is_not_valid);
+	tcase_add_test(subtract_case, given_MMMM_and_I_when_subtract_then_MMMM_is_not_valid);
 	return subtract_case;
 }
 

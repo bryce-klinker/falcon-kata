6306e118ce02f19ab6645cbeb8d1891c1ef49526
vagrant <vagrant@ubuntu1404-desktop.localdomain>
2016-08-08 15:33:19 -0500

    Added errno check to too many M's test

--- tests/roman_validator_suite.c
+++ tests/roman_validator_suite.c
@@ -30,6 +30,7 @@ END_TEST
 START_TEST(given_MMMM_when_is_roman_valid_then_false) {
 	bool isValid = is_roman_valid("MMMM");
 	ck_assert(isValid == false);
+	ck_assert_int_eq(ROMAN_NUMERAL_LIMIT, errno);
 }
 END_TEST
 

c6f23a04cd77c12676cc9f3af2a6bbf7bd4a4686
vagrant <vagrant@ubuntu1404-desktop.localdomain>
2016-08-08 11:27:58 -0500

    Added test cases to roman validator Calculator suite was getting hard to navigate

--- tests/roman_validator_suite.c
+++ tests/roman_validator_suite.c
@@ -4,12 +4,34 @@
 #include "../src/roman_validator.h"
 #include "roman_validator_suite.h"
 
+START_TEST(given_IIII_when_is_roman_valid_then_false) {
+	bool isValid = is_roman_valid("IIII");
+	ck_assert(isValid == false);
+}
+END_TEST
+
+START_TEST(given_XXXX_when_is_roman_valid_then_false) {
+	bool isValid = is_roman_valid("XXXX");
+	ck_assert(isValid == false);
+}
+END_TEST
+
+START_TEST(given_MMMM_when_is_roman_valid_then_false) {
+	bool isValid = is_roman_valid("MMMM");
+	ck_assert(isValid == false);
+}
+END_TEST
+
 Suite * roman_validator_suite(void) {
 	Suite *suite;
 	TCase *testCase;
 
 	suite = suite_create("Roman Validator");
 	testCase = tcase_create("Is Roman Valid");
+
+	tcase_add_test(testCase, given_IIII_when_is_roman_valid_then_false);
+	tcase_add_test(testCase, given_XXXX_when_is_roman_valid_then_false);
+	tcase_add_test(testCase, given_MMMM_when_is_roman_valid_then_false);
 
 	suite_add_tcase(suite, testCase);
 	return suite;

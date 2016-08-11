e5d1b0b85a108ab0463305ada94129a94d8b87c2
vagrant <vagrant@ubuntu1404-desktop.localdomain>
2016-08-11 16:24:04 -0500

    Fixing invalid roman numeral to arabic bug

--- src/roman_to_arabic.c
+++ src/roman_to_arabic.c
@@ -2,8 +2,13 @@
 #include <string.h>
 #include <stdbool.h>
 #include "roman_arabic_map.h"
+#include "roman_validator.h"
 
 int to_arabic(const char *roman) {
+	if (!is_roman_valid(roman)) {
+		return -1;
+	}
+
 	int value = 0;
 	int length = strlen(roman);
 	for(int i = 0; i < length; ++i) {
